b0024692d12a6f31c374ad99f715ee20267eb018
Julien Leroy <jleroy@gingerminds.fr>
2024-07-26 18:43:15 +0200

    feat: simplify tree generation & make it recursive

--- resources/views/move.blade.php
+++ resources/views/move.blade.php
@@ -1,30 +1,46 @@
+@php
+    function renderSubdirectories($directories, $items, $level = 1) {
+        foreach ($directories as $directory) {
+            $class = 'level-' . $level;
+            echo '<li class="nav-item sub-item ' . $class . '">';
+            echo '<a class="nav-link" href="#" data-type="0" onclick="moveToNewFolder(`' . $directory->url . '`)">';
+            echo '<i class="fa fa-folder fa-fw"></i> ' . $directory->name;
+            echo '<input type="hidden" id="goToFolder" name="goToFolder" value="' . $directory->url . '">';
+            echo '<div id="items">';
+            foreach ($items as $i) {
+                echo '<input type="hidden" id="' . $i . '" name="items[]" value="' . $i . '">';
+            }
+            echo '</div>';
+            echo '</a>';
+            if (isset($directory->children)) {
+                echo '<ul class="nav nav-pills flex-column">';
+                renderSubdirectories($directory->children, $items, $level + 1);
+                echo '</ul>';
+            }
+            echo '</li>';
+        }
+    }
+@endphp
+
 <ul class="nav nav-pills flex-column">
-  @foreach($root_folders as $root_folder)
-    <li class="nav-item">
-      <a class="nav-link" href="#" data-type="0" onclick="moveToNewFolder(`{{$root_folder->url}}`)">
-        <i class="fa fa-folder fa-fw"></i> {{ $root_folder->name }}
-        <input type="hidden" id="goToFolder" name="goToFolder" value="{{ $root_folder->url }}">
-        <div id="items">
-          @foreach($items as $i)
-            <input type="hidden" id="{{ $i }}" name="items[]" value="{{ $i }}">
-          @endforeach
-        </div>
-      </a>
-    </li>
-    @foreach($root_folder->children as $directory)
-    <li class="nav-item sub-item">
-      <a class="nav-link" href="#" data-type="0" onclick="moveToNewFolder(`{{$directory->url}}`)">
-        <i class="fa fa-folder fa-fw"></i> {{ $directory->name }}
-        <input type="hidden" id="goToFolder" name="goToFolder" value="{{ $directory->url }}">
-        <div id="items">
-          @foreach($items as $i)
-            <input type="hidden" id="{{ $i }}" name="items[]" value="{{ $i }}">
-          @endforeach
-        </div>
-      </a>
-    </li>
+    @foreach($root_folders as $root_folder)
+        <li class="nav-item">
+            <a class="nav-link" href="#" data-type="0" onclick="moveToNewFolder(`{{$root_folder->url}}`)">
+                <i class="fa fa-folder fa-fw"></i> {{ $root_folder->name }}
+                <input type="hidden" id="goToFolder" name="goToFolder" value="{{ $root_folder->url }}">
+                <div id="items">
+                    @foreach($items as $i)
+                        <input type="hidden" id="{{ $i }}" name="items[]" value="{{ $i }}">
+                    @endforeach
+                </div>
+            </a>
+        </li>
+        @if(isset($root_folder->children))
+            <ul class="nav nav-pills flex-column">
+                @php renderSubdirectories($root_folder->children, $items, 1); @endphp
+            </ul>
+        @endif
     @endforeach
-  @endforeach
 </ul>
 
 <script>
